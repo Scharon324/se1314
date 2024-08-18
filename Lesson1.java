a1a803e44039a4a6ce6941a85cc63056f6627502
Scharon324 <scharon324@protonmail.com>
2024-08-18 15:39:04 +0300

    Lessons from UNI and just exercising.

---
+++ Lesson1.java
@@ -0,0 +1,33 @@
+public class Lesson1 /*public class means your name of the program, you are not allow to put numbers Incorrect
+ file name examples:
+1234,Hello World, 100 helloworld. Correct examples: Hello_world, SE1235, HelloWorld.*/
+
+{
+
+public static void main(String [] args){
+/*You need to remember those two parts because all the programming languages has these commands written its
+ essential: 
+public class Lesson1 { public static void main(String [] args) } */
+    System.out.println("Hello World");
+    System.out.println("I am good thank you very much.");
+    System.out.println("ABC " + "Why not?");
+    // Be careful for ABC
+    //It is very dangerous
+    //+ is conansiation (Join something together) the same thing in maths.
+
+    System.out.println((5+5)*2);
+    System.out.println("ABC" + (6+6));
+    // Each line in the program must end with a ;
+    //We only use three brackets in Java these are: [],{},().
+    //
+    /*This is also a comment command example-: /*Example*/
+    //System.out.println("/f");- The purpose of this code is to clear the terminal window.
+    //println- means to write the text into the terminal window (basically it will create space line)
+    //System.out.print ("sdafsf");- without print(ln) means that it will continue text after this message to
+    // the next text message.
+    //------------------------------------------------------------------
+
+}
+
+
+}
