a7e9e96c7c9cb475294698aa8f655347db95602f
Scharon324 <scharon324@protonmail.com>
2024-08-21 16:27:49 +0300

    Continuation for later

--- Lesson_8b.java
+++ Lesson_8b.java
@@ -1,12 +1,38 @@
+import java.util.*;
+
 public class Lesson_8b {
 // Ask for four numbers to be entered and display the largest one and the smallest one.
+static Scanner sc;    
+public static void main(String [] args){
 
-    public static void main(String [] args){
+//Numbers as double strings
+double firstNumber, secondNumber, thirdNumber, fourthNumber;
+
+//First number to print out.
+Scanner sc = new Scanner(System.in);
+System.out.println("Please enter your first number: ");
+firstNumber = Integer.parseInt(sc.nextLine());
+
+//Second Number
+
+System.out.println("Pleas enter you're second number: ");
+secondNumber = Integer.parseInt(sc.nextLine());
+
+//Third Number
+
+System.out.println("Please enter your third number: ");
+thirdNumber = Integer.parseInt(sc.nextLine());
+
+//Fourth Number
+
+System.out.println("Please Enter your fourth number: ");
+fourthNumber = Integer.parseInt(sc.nextLine());
+
+if((firstNumber > secondNumber) && (firstNumber > thirdNumber) && (firstNumber > fourthNumber)){
+    System.out.println("you're largest number is: " + firstNumber);
 
 
-
-
-
+}
 
 
     }
