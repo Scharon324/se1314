ac30a9b26e8890fc53ab72893386e2beda8c1d4f
Scharon324 <scharon324@protonmail.com>
2024-08-27 18:04:51 +0300

    Added new lessons

--- Lesson_12Ex1.java
+++ Lesson_12Ex1.java
@@ -1,18 +1,22 @@
+import java.util.*;
 public class Lesson_12Ex1 {
-
+// print 0,1,3,6,10
+static Scanner sc;
     public static void main(String[] args) {
         
+        int a = 0;
+        int b = 1;
+
+        while (a <= 10) {
+            System.out.println(a);
+            a= a+b;
+            b++;
+
+        }
 
 
 
-        
+
     }
 
-
-
-
-
-
-
-    
 }
